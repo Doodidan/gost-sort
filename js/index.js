a73dff3cdf8dccc3a35b7562cb4af525c548db25
Gosha Egorian <gosha.egorian@gmail.com>
2019-06-13 01:37:57 +0300

    Add data process functions

--- js/index.js
+++ js/index.js
@@ -1,6 +1,30 @@
-let input = document.querySelector("textarea");
+const input = document.querySelector("textarea");
+const resultBlock = document.querySelector(".result");
 
-let mokka = ["Sort", "Привет", "Папа", "Data"];
+const engReg = /[a-zA-Z]/;
+const rusReg = /[а-яА-Я]/;
+const numReg = /^\d\. /;
+const newlineReg = /\s*\n\s*/;
+
+function process() {
+  const arr = parseArray(input.value);
+  arr.sort(sortArray);
+  writeText(arr);
+}
+
+function writeText(array) {
+  resultBlock.innerHTML = array
+    .map((item, index) => index + 1 + ". " + item)
+    .join("<br />");
+}
+
+function parseArray(str) {
+  let arr = str.split(newlineReg);
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = arr[i].replace(numReg, "");
+  }
+  return arr;
+}
 
 function sortArray(el1, el2) {
   if (compareLangs(el1[0], el2[0])) {
@@ -8,9 +32,6 @@ function sortArray(el1, el2) {
   }
   return rusReg.test(el2) ? 1 : -1;
 }
-
-const engReg = /[a-zA-Z]/;
-const rusReg = /[а-яА-Я]/;
 
 function compareLangs(a, b) {
   if (
@@ -21,7 +42,3 @@ function compareLangs(a, b) {
   }
   return false;
 }
-
-mokka.sort(sortArray);
-
-console.log(mokka);
